fddde18142622c7058aec1e57904ebba7f7a16f8
rafaellucas30290 <orafaellucas5@gmail.com>
2025-04-07 16:48:37 -0300

    First Commit

---
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,20 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+    @Autowired
+    AlunoService alunoService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void CriarAluno(@RequestBody Aluno aluno){
+        alunoService.criarAluno(aluno);
+    }
+
+}
